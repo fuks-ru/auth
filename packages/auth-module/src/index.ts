8426fcc2c9549bd5e40ac92ee90907076ee44c8b
Dmitry Fuks <difuks@gmail.com>
2022-10-20 02:21:21 +0300

    fix: guards

--- packages/auth-module/src/index.ts
+++ packages/auth-module/src/index.ts
@@ -1,3 +1,4 @@
 export { Roles } from 'auth-module/decorators/Roles';
 export { RolesGuard } from 'auth-module/guards/RolesGuard';
 export { AuthModule } from 'auth-module/AuthModule';
+export { AuthJwtGuard } from 'auth-module/guards/AuthJwtGuard';
