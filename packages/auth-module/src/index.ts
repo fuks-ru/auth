7470ede75a5f8a2da3460c756fd3b99d92b39b32
Dmitry Fuks <difuks@gmail.com>
2022-10-20 01:56:03 +0300

    fix: remove unused guard

--- packages/auth-module/src/index.ts
+++ packages/auth-module/src/index.ts
@@ -1,4 +1,3 @@
 export { Roles } from 'auth-module/decorators/Roles';
-export { Public } from 'auth-module/decorators/Public';
 export { RolesGuard } from 'auth-module/guards/RolesGuard';
 export { AuthModule } from 'auth-module/AuthModule';
