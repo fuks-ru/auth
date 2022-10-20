f5b50c1da494d96eaaa104abc26246fe0e85efdb
Dmitry Fuks <FuksD@llcglobaladvisors.com>
2022-10-20 22:29:07 +0300

    fix: export auth client

--- packages/auth-module/src/AuthModule.ts
+++ packages/auth-module/src/AuthModule.ts
@@ -24,6 +24,7 @@ export class AuthModule {
           useValue: options,
         },
       ],
+      exports: [AuthClient],
     };
   }
 
@@ -44,6 +45,7 @@ export class AuthModule {
           useFactory: options.useFactory,
         },
       ],
+      exports: [AuthClient],
     };
   }
 }
